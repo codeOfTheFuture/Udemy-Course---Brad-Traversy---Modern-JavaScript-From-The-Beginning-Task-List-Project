78f42773f0623286dc082866e7f46dc3b2f6b7fe
Jeff Oliver <jeffoliver3384@gmail.com>
2018-10-14 23:42:31 -0400

    Persist to Local Storage/Added some code of my own that fixed a bug - added a return after the alert when Add Task button is clicked without a task in the input field because an empty li would appear

--- js/app.js
+++ js/app.js
@@ -10,6 +10,8 @@ loadEventListeners();
 
 // Load All Event Listeners
 function loadEventListeners() {
+  // DOM load event
+  document.addEventListener('DOMContentLoaded', getTasks);
   // Add task event
   form.addEventListener('submit', addTask);
   // Remove task event
@@ -20,13 +22,43 @@ function loadEventListeners() {
   filter.addEventListener('keyup', filterTasks);
 }
 
+// Get Tasks from Local Storage
+function getTasks() {
+  let tasks;
+  if(localStorage.getItem('tasks') === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+  }
+
+  tasks.forEach((task) => {
+    // Creat li element
+  const li = document.createElement('li');
+  // Add class
+  li.className = 'collection-item';
+  // Create text node and append to li
+  li.appendChild(document.createTextNode(task));
+  // Create new link element
+  const link = document.createElement('a');
+  // Add class
+  link.className = 'delete-item secondary-content';
+  // Add icon html
+  link.innerHTML = '<i class = "fa fa-remove"></i>';
+  // Append the link to li
+  li.appendChild(link);
+
+  // Append the li to ul
+  taskList.appendChild(li);
+  });
+}
+
 // Add Task
 function addTask(e) {
   if(taskInput.value === '') {
     alert('Add a Task');
-  }
-
-  // Creat li element
+    return;
+  } else {
+    // Creat li element
   const li = document.createElement('li');
   // Add class
   li.className = 'collection-item';
@@ -44,10 +76,28 @@ function addTask(e) {
   // Append the li to ul
   taskList.appendChild(li);
 
+  // Store task in Local Storage
+  storeTaskInLocalStorage(taskInput.value);
+
   // Clear the input
   taskInput.value = '';
+  }
 
   e.preventDefault();
+}
+
+// Store Task
+function storeTaskInLocalStorage(task) {
+  let tasks;
+  if(localStorage.getItem('tasks') === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+  }
+
+  tasks.push(task);
+
+  localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
 // Remove Task
@@ -55,8 +105,29 @@ function removeTask(e) {
   if(e.target.parentElement.classList.contains('delete-item')) {
     if(confirm('Are you sure?')) {
       e.target.parentElement.parentElement.remove();
+
+      // Remove from Local Storage
+      removeTaskFromLocalStorage(e.target.parentElement.parentElement);
     }
   }
+}
+
+// Remove from Local Storage
+function removeTaskFromLocalStorage(taskItem) {
+  let tasks;
+  if(localStorage.getItem('tasks') === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+  }
+
+  tasks.forEach((task, index) => {
+    if(taskItem.textContent === task) {
+      tasks.splice(index, 1);
+    }
+  });
+
+  localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
 // Clear Tasks
@@ -64,13 +135,23 @@ function clearTasks() {
   // taskList.innerHTML = '';
 
   //Faster
-  if(confirm('Are you sure?')) {
-    while(taskList.firstChild) {
-      taskList.removeChild(taskList.firstChild);
+  if (localStorage.getItem('tasks')) {
+    if(confirm('Are you sure?')) {
+      while(taskList.firstChild) {
+        taskList.removeChild(taskList.firstChild);
+      }
     }
-  }
+  }  
 
   // https://jsperf.com/innerhtml-vs-removechild/47
+
+  //Clear Tasks from Local Storage
+  clearTasksFromLocalStorage();
+}
+
+// Clear Tasks from Local Storage
+function clearTasksFromLocalStorage() {
+  localStorage.clear();
 }
 
 // Filter Tasks
